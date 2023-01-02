cbba66edf4d9db61c2ee54f70edd6516195bd73d
Adi Biton <adi.biton@gmail.com>
2023-01-02 23:06:36 +0200

    feat(): first commit

---
+++ Controllers/TodoApiController.cs
@@ -0,0 +1,32 @@
+namespace TodoApi.Controllers;
+
+using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[Route("[controller]")]
+public class TodoApiController : ControllerBase
+{
+    private static readonly string[] Summaries = new[]
+    {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+    private readonly ILogger<TodoApiController> _logger;
+
+    public TodoApiController(ILogger<TodoApiController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet(Name = "GetWeatherForecast")]
+    public IEnumerable<WeatherForecast> Get()
+    {
+        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        {
+            Date = DateTime.Now.AddDays(index),
+            TemperatureC = Random.Shared.Next(-20, 55),
+            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+        })
+        .ToArray();
+    }
+}
